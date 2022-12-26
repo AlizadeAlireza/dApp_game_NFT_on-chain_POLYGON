0d271158e44ae57e6fa73d06a97472236d00b91a
alireza alizadeh <alireza.aa9700@gmail.com>
2022-12-26 16:51:58 +0330

    add_getLevels&&getTokenURI

--- README.md
+++ README.md
@@ -11,3 +11,22 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
 ```
+
+### why we use SVG
+
+we can display base64 images in the browser without the need for a hosting provider.
+
+Copying and pasting the svg code and data: in your browser URL bar will display the same image.
+
+and it's not a common URL:
+
+    - Data directives
+    - The Base64 data - contains the actual data
+
+This is useful because, even if Solidity is not able to handle images, it is able to handle strings and SVGs aren't anything else than sequences of tags and strings we can easily retrieve runtime, plus, converting everything to base64, will allow us to store our images on-chain without the need of object storage.
+
+## functions
+
+-   generateCharacter():
+
+    for get the SVG file to it and get token id to levelup.
--- contracts/chainBattles.sol
+++ contracts/chainBattles.sol
@@ -9,9 +9,9 @@ import "@openzeppelin/contracts/utils/Strings.sol"; // converting int to str
 import "@openzeppelin/contracts/utils/Base64.sol"; // work with our SVGs
 
 contract ChianBattles is ERC721URIStorage {
-    using strings for uint256;
+    using Strings for uint256;
     using Counters for Counters.Counter;
-    Counter.Counter private _tokenIds;
+    Counters.Counter private _tokenIds;
 
     // keeping track of the levels for each of our NFT's.
     mapping(uint256 => uint256) public tokenIdtoLevels;
@@ -26,6 +26,27 @@ contract ChianBattles is ERC721URIStorage {
             '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">Warrior</text>',
             '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">Levels: getLevels(tokenId)</text>',
             "</svg>"
+        );
+
+        return string(abi.encodePacked("data:image/svg+xml;base64,", Base64.encode(svg)));
+    }
+
+    function getLevels(uint256 tokenId) public view returns (string memory) {
+        uint256 levels = tokenIdtoLevels[tokenId];
+        return levels.toString();
+    }
+
+    function getTokenURI(uint256 tokenId) public returns (string memory) {
+        bytes memory dataURI = abi.encodePacked(
+            "{",
+            '"name": "Chain Battles #',
+            tokenId.toString(),
+            '",',
+            '"description": "Battles on chain with alireza",',
+            '"image": "',
+            generateCharacter(tokenId),
+            '"',
+            "}"
         );
     }
 }
